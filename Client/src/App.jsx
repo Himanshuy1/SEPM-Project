2a1f33b2e9bc45ad09ff67231c87c4ed9978788a
Himanshu Yadav <hy140064@gmail.com>
2026-03-18 14:52:20 +0530

    feat(client): setup base react application and routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,7 +12,13 @@ import CreateDoubt from "./pages/CreateDoubt";
 import AnswerDoubt from "./pages/AnswerDoubt";
 import DoubtDetails from "./pages/DoubtDetails";
 import Resources from "./pages/Resources";
+import Summariser from "./pages/Summariser";
+import Chatbot from "./pages/Chatbot";
 import Profile from "./pages/Profile";
+
+
+import { SocketProvider } from "./context/SocketContext";
+import Chat from "./pages/Chat";
 
 
 // 🔹 Redirect authenticated users away from login/register
@@ -62,7 +68,11 @@ function AppRoutes() {
         <Route path="/doubts/:id" element={<DoubtDetails />} />
         <Route path="/doubts/:id/answer" element={<AnswerDoubt />} />
         <Route path="/resources" element={<Resources />} />
+        <Route path="/summariser" element={<Summariser />} />
+        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/chat/:userId" element={<Chat />} />
       </Route>
 
     </Routes>
@@ -73,9 +83,11 @@ function AppRoutes() {
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <SocketProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </SocketProvider>
     </AuthProvider>
   );
 }
